a48d1c281042730a3eb5b74436b428e9ca4c67b0
navyuth <34467822+navyuth@users.noreply.github.com>
2017-12-19 15:12:17 +0530

    Add files via upload

--- OddPallindrome.java
+++ OddPallindrome.java
@@ -4,21 +4,28 @@ public class OddPallindrome {
 
 	public static void main(String[] args) {
 		
-		int number = 1551,rev;
+		int number = 1221,rev;
 		
 		if(checkAllDigitsOdd(number)){
 			rev = reverse(number);
+			
 			if(checkPallindrome(number,rev)) {
+				
 				System.out.println("Number is Odd Pallindrome");
+			
 			}
+			
 			else {
+			
 				System.out.println("Number is not Odd Pallindrome");
 			}
 		}
+			else {
+				
+				System.out.println("Number is not Odd Pallindrome");
+			}
 		
-	
-
-	}
+		}
 	
 	 public static boolean checkAllDigitsOdd(int number) {
 		 
@@ -26,29 +33,41 @@ public class OddPallindrome {
 		 int count =0;
 		 
 		 while(number >0) {
+		
 			 digit = number%10;
 			 number = number/10;
 			 if(digit%2 == 0) 
 			  count++;
 		 }
+		 
 		 if(count == 0) {
 			 return true;
+		 
 		 }
+		 
 		 else {
+		
 			 return false;
+	
 		 }
+	 
 	 }
+	 
 	 public static int reverse(int number) {
+		 
 		 int reverse = 0;
 		 
 		 while(number>0) {
+			 
 			 int digit = number%10;
 			 number = number/10;
 			 reverse = reverse * 10 + digit;
 			 
 		 }
+		 
 		 return reverse;
 	 }
+	 
 	 public static boolean checkPallindrome(int number,int reverse) {
 		 
 		 if(number == reverse) {
