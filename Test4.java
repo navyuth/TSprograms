6a2eecccd3cbeec5b837714f0de1215855841c80
navyuth <34467822+navyuth@users.noreply.github.com>
2018-01-05 18:06:06 +0530

    Add files via upload

--- Test4.java
+++ Test4.java
@@ -4,13 +4,22 @@ public class Test4
 {
 	public static void main(String args[])
 	{
-		List list= new ArrayList();
+		List<Integer> list= new ArrayList<Integer>();
 		//System.out.println(list);
 		list.add(10);
 		list.add(20);
 		list.add(52);
 		list.add(53);
 		list.add(108);
-		System.out.println(list);
+				for(int i= 0; i<list.size();i++)
+		{
+			System.out.println(list.get(i));
+		}
+		System.out.println("===========");
+		//display all the elements by using for-each
+		for(Integer a: list)
+		{
+			System.out.println(a);
+		}
 	}
 }
