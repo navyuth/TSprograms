2a14e32f4911c0e0238e6ef3a1bc36f26612b290
navyuth <34467822+navyuth@users.noreply.github.com>
2018-01-05 17:46:23 +0530

    Add files via upload

--- Test4.java
+++ Test4.java
@@ -1,26 +1,16 @@
+import java.util.*;
 
-public class Test4 {
-
-	public static void main(String[] args)
+public class Test4
+{
+	public static void main(String args[])
 	{
-		String str = "talentsprint2017@";
-		String s1 = "";
-		String s2 = "";
-		String s = "";
-		for(int i = 1; i<=str.length();i++)
-		{
-			char ch = str.charAt(i);
-			if(Character.isLetter(ch))
-			{
-				s1 = s1 + str.toUpperCase();
-			}
-			else
-			{
-				s2 = s2 + str.charAt(i);
-			}
-		}
-		System.out.println(s);
-
+		List list= new ArrayList();
+		//System.out.println(list);
+		list.add(10);
+		list.add(20);
+		list.add(52);
+		list.add(53);
+		list.add(108);
+		System.out.println(list);
 	}
-
 }
